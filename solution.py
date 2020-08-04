506ff4c2028c19bdaf62ce2604afa74ddacb5853
Eryoung-610 <eryoung610@gmail.com>
2020-08-04 16:36:59 -0700

    Part 1 completed

---
+++ solution.py
@@ -0,0 +1,20 @@
+import geocoder
+
+destinations = [
+    "The Space Needle",
+    "The Golden Gate Bridge",
+    "Yosemite National Park",
+    "Las Vegas, Nevada",
+    "Grand Canyon National Park",
+    "Aspen,Colorado",
+    "Mount Rushmore",
+    "Yellowstone National Park",
+    "Sandpoint, Idaho",
+    "Banff National Park",
+    "Capilano Suspension Bridge"
+]
+
+for point in destinations:
+    location = geocoder.arcgis(point)
+
+    print("{0} is located at ({1:.4f}, {2: .4f})".format(point, location.latlng[0], location.latlng[1]))
